117510fe1e25d885081392575754573989e2cc5a
HY-WG <mung94c@gmail.com>
2025-09-02 14:02:58 +0900

    초기 커밋

---
+++ src/main/java/org/example/taskflowd/TaskflowDApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskflowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskflowDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/taskflowd/TaskflowDApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taskflowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
