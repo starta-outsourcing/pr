60079ffc2726dc617eeaf4d4538ab466b6400c0e
OverNight <hidden98716@gmail.com>
2025-09-04 09:23:45 +0900

    feat(TSK-007) : Add some DTO And Descriptor

--- src/main/java/org/example/taskflowd/domain/task/dto/TaskDescriptor.java
+++ src/main/java/org/example/taskflowd/domain/task/dto/TaskDescriptor.java
@@ -1,5 +1,6 @@
 package org.example.taskflowd.domain.task.dto;
 
+import org.example.taskflowd.domain.task.dto.response.TaskCreateResponse;
 import org.example.taskflowd.domain.task.enums.TaskPriority;
 import org.example.taskflowd.domain.task.enums.TaskStatus;
 
@@ -16,4 +17,17 @@ public record TaskDescriptor(
         LocalDateTime dueDate,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
-) {}
+) {
+    public static TaskDescriptor toDescriptor(Long id,
+                                              String title,
+                                              String description,
+                                              Long writerId,
+                                              Long assigneeId,
+                                              TaskStatus status,
+                                              TaskPriority priority,
+                                              LocalDateTime dueDate,
+                                              LocalDateTime createdAt,
+                                              LocalDateTime updatedAt) {
+        return new TaskDescriptor(id, title, description, writerId, assigneeId, status, priority, dueDate, createdAt, updatedAt);
+    }
+}
